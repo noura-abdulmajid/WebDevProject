9def580a962bc31d788f949849d13186e62b34a2
Charlie Garfield <240380300@aston.ac.uk>
2025-02-13 12:50:45 +0000

    Added Returns Model and Migrations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
-    public function ordered_items(): HasMany
+    public function ordered_item(): HasMany
     {
         return $this->hasMany(OrderedItem::class);
     }
@@ -16,5 +17,10 @@ class Order extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(Customer::class);
+    }
+
+    public function returns(): HasOne
+    {
+        return $this->hasOne(Returns::class);
     }
 }
