be8fb858f21e8776a6b45a21a37a0c095d479b7a
scotthuang1129 <scotthuang1129@gitlab.com>
2025-03-04 14:37:44 +0000

    delete the frontend

--- Frontend/src/components/AdminSettings.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Admin Setting</h1>
-</template>
--- Frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import "./styles/GlobalStyle.css";  // login styles
-
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router"; // Import the router
-
-const app = createApp(App);
-app.use(router); // Use the router
-app.mount("#app");
